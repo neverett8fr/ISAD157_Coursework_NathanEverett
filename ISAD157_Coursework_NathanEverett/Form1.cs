7732b9668d447f2c3a3cdfb2c01bf97a31d8c724
Nathan Everett <nathan.everett@students.plymouth.ac.uk>
2020-05-01 05:32:01 +0100

    connected to database -- prototype customise inputs to do input from excel to do

--- ISAD157_Coursework_NathanEverett/Form1.cs
+++ ISAD157_Coursework_NathanEverett/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,41 @@ namespace ISAD157_Coursework_NathanEverett
 
         void connectMySQL()
         {
+            string connstring = @"server=proj-mysql.uopnet.plymouth.ac.uk;userid=ISAD157_NEverett;password=ISAD157_22225597;database=isad157_neverett";
+            MySqlConnection connection = null;
+
+            try
+            {
+                connection = new MySqlConnection(connstring);
+                connection.Open();
+
+                string query = "SELECT * FROM table_user;";
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter(query, connection);
+                DataSet dataSet = new DataSet();
+                dataAdapter.Fill(dataSet, "table_user");
+                DataTable dataTable = dataSet.Tables["table_user"];
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    foreach (DataColumn col in dataTable.Columns)
+                    {                        
+                        MessageBox.Show(row[col] + "\t");
+                    }
+
+                    MessageBox.Show("\n");
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: {0}", e.ToString());
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
 
         }
         private void BTNSelectDataSource_Click(object sender, EventArgs e)
